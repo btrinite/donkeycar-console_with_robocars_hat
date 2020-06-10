7cfdd9e63af66a80b00381191316688f19993c09
Jason Leung <jasonleung101@gmail.com>
2020-06-10 16:54:04 +0800

    Fix: - first time setup wifi --> should reboot

--- dkconsole/vehicle/services.py
+++ dkconsole/vehicle/services.py
@@ -344,7 +344,7 @@ class Vehicle(object):
             wifi = cls.add_network(ssid, psk)
             if wifi is not True:
                 raise Exception("Cannot connect to wifi")
-            cls.reboot_required = False
+            cls.reboot_required = True
 
         if hostname is not None:
             cls.set_hostname(hostname, None)
