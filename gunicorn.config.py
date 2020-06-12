d6526c87e4a46fcd7166547a9b4238c237696aa3
Jonathan Tse <jonathan@tsez.net>
2020-06-12 17:02:30 +0800

    change to threads in gunicorn settings

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -3,4 +3,5 @@ errorlog = 'gunicorn.log'
 capture_output = True
 timeout=180
 access_log_format='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(T)s %(p)s'
-workers=4
+# workers=4
+threads=4
