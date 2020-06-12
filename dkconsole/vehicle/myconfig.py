2b0783ba033668e1a149b219c4f6d680cd764228
Jonathan Tse <jonathan@tsez.net>
2020-06-12 17:13:06 +0800

    add myconfig.py for reset

---
+++ dkconsole/vehicle/myconfig.py
@@ -0,0 +1,12 @@
+MM1_STEERING_MID = 1480
+MM1_MAX_FORWARD = 1700  # Max is 2000
+MM1_MAX_REVERSE = 1200
+MM1_STOPPED_PWM = 1500
+MM1_SHOW_STEERING_VALUE = False
+MM1_SERIAL_PORT = '/dev/ttyS0'
+
+
+DRIVE_TRAIN_TYPE = "MM1" # SERVO_ESC|DC_STEER_THROTTLE|DC_TWO_WHEEL|SERVO_HBRIDGE_PWM
+
+CONTROLLER_TYPE='F710'               #(ps3|ps4|xbox|nimbus|wiiu|F710|rc3)
+JOYSTICK_DEADZONE = 0.11             # when non zero, this is the smallest throttle before recording triggered.
